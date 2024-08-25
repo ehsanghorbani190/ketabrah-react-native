88553ae711fa707e65bcc29b795c1947e52d6f97
Ehsan Ghorbani <t1m3runn3r2359@protonmail.com>
2024-08-25 11:32:46 +0330

    [feat] finalized colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,23 +3,30 @@ import {Theme} from '@react-navigation/native';
 export const LightTheme: Theme = {
   dark: false,
   colors: {
-    primary: '#79AC78',
-    background: '#FFFFFF',
-    card: '#F6F6F6',
-    text: '#000',
-    border: '#808080',
-    notification: '#FFFFFF',
+    primary: '#5E9527',
+    background: '#F9F9F9',
+    card: '#F9F9F9',
+    text: '#333',
+    border: '#BDBDBD',
+    notification: '#F9F9F9',
   },
 };
 
 export const DarkTheme: Theme = {
   dark: true,
   colors: {
-    primary: '#A7D397',
-    background: '#353B48',
-    card: '#2F3640',
-    text: '#F5F6FA',
-    border: '#DCDDE1',
-    notification: '#353B48',
+    primary: '#4CAF50',
+    background: '#121212',
+    card: '#1E1E1E',
+    text: '#FFFFFF',
+    border: '#424242',
+    notification: '#121212',
+  },
+};
+
+export const CustomColors = {
+  light: {},
+  dark: {
+    inactive_gray: '#999999',
   },
 };
