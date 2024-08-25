3a55430a9fadb14225848828a9b374c2547a5b41
Ehsan Ghorbani <t1m3runn3r2359@protonmail.com>
2024-08-25 19:30:29 +0330

    [feat] added logo

--- constants/Images.ts
+++ constants/Images.ts
@@ -1,4 +1,5 @@
 const typography = require('@/assets/images/typography.png');
 const logoWithTypography = require('@/assets/images/logo_with_typography.png');
+const logo = require('@/assets/images/logo.png');
 
-export default {typography, logoWithTypography};
+export default {typography, logoWithTypography, logo};
