cb585a4588ba22b83cd5b36bb6c50481030e44cb
Ehsan Ghorbani <t1m3runn3r2359@protonmail.com>
2024-08-25 11:35:51 +0330

    [chore] cleaned up themed text

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -1,11 +1,13 @@
 import {TextProps, Text} from 'react-native';
 import {useTheme} from '@react-navigation/native';
 
-export default function ThemedText(props: TextProps) {
+export default function ThemedText({children, style, ...rest}: TextProps) {
   const theme = useTheme();
-  const {children, ...rest} = props;
   return (
-    <Text style={{color: theme.colors.text, fontFamily: 'Vazir'}} {...rest}>
+    <Text
+      style={[{color: theme.colors.text, fontFamily: 'Vazir'}, style]}
+      {...rest}
+    >
       {children}
     </Text>
   );
