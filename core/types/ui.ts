6ee5c2052a94b397913d2521c6f95e1c4fbe9b19
Ehsan Ghorbani <t1m3runn3r2359@protonmail.com>
2024-08-29 12:36:41 +0330

    [feat] add reset state to to stack header

--- core/types/ui.ts
+++ core/types/ui.ts
@@ -24,6 +24,7 @@ export interface ITabBarLabelProps {
 
 export interface IStackScreenHeaderProps extends NativeStackHeaderProps {
   title: string;
+  resetState?: Function;
 }
 
 export interface IInputWithLabelProps {
