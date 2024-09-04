4760872ef1a6fbd5671210ecc88e19112c968d75
Ehsan Ghorbani <t1m3runn3r2359@protonmail.com>
2024-09-04 10:31:24 +0330

    [feat] scroll view should start from right to left when is horizontal

--- components/ThemedScrollView.tsx
+++ components/ThemedScrollView.tsx
@@ -1,14 +1,24 @@
 import {ScrollView, ScrollViewProps} from 'react-native';
 import {useTheme} from '@react-navigation/native';
+import {useEffect, useRef} from 'react';
 
 export default function ThemedScrollView({
   children,
   style,
+  contentContainerStyle,
   ...rest
 }: ScrollViewProps) {
+  const ref = useRef<ScrollView | null>(null);
   const theme = useTheme();
+  useEffect(() => {
+    if (ref.current && rest.horizontal) {
+      ref.current.scrollToEnd({animated: false});
+    }
+  }, []);
   return (
     <ScrollView
+      ref={ref}
+      contentContainerStyle={contentContainerStyle}
       style={[{backgroundColor: theme.colors.background}, style]}
       {...rest}
     >
