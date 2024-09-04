efbd8994311fcda4dc968d523262a6bfad245aa9
Ehsan Ghorbani <t1m3runn3r2359@protonmail.com>
2024-09-04 10:28:10 +0330

    [feat] added banner image

--- constants/Images.ts
+++ constants/Images.ts
@@ -1,5 +1,6 @@
 const typography = require('@/assets/images/typography.png');
 const logoWithTypography = require('@/assets/images/logo_with_typography.png');
 const logo = require('@/assets/images/logo.png');
+const bannerOff = require('@/assets/images/banner_off.jpg');
 
-export default {typography, logoWithTypography, logo};
+export default {typography, logoWithTypography, logo, bannerOff};
